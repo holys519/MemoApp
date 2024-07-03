8210cd843694fbe15306c0378226e01d078f4bf8
holy <68938511+holys519@users.noreply.github.com>
2024-07-04 01:18:32 +0900

    icomoon font add

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,8 @@
-import { View, StyleSheet } from "react-native";
+import { Redirect } from "expo-router";
 
-import Header from "../components/Header";
-import MemoListItem from "../components/MemoListItem";
-import CircleButton from "../components/CircleButton";
-
-const Index = (): JSX.Element => {
-    return (
-        <View style= {styles.container}>
-            <Header/>
-            <View>
-                <MemoListItem/>
-                <MemoListItem/>
-                <MemoListItem/>
-            </View>
-            <CircleButton children={"+"}/>
-        </View>
-    )
+const Index = () => {
+    return <Redirect href='memo/list' />
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffffff'
-    }
-})
-
 export default Index
+
